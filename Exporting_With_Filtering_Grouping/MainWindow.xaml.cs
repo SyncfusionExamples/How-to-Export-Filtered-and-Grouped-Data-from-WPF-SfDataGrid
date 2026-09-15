c1088e632ae999850cb8ffa9430cf7c2ec4148de
GowthamabalajiSambathSF4978 <gowthamabalaji.sambath@syncfusion.com>
2026-09-15 11:44:48 +0530

    Ticket-873237: Export Filtered and Grouped data in WPF SfDataGrid.

---
+++ Exporting_With_Filtering_Grouping/MainWindow.xaml.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.Win32;
+using Syncfusion.UI.Xaml.Grid.Converter;
+using Syncfusion.XlsIO;
+using System.IO;
+using System.Windows;
+
+namespace Exporting_With_Filtering_Grouping
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnExportToExcel(object sender, RoutedEventArgs e)
+        {
+            var options = new ExcelExportingOptions();
+            options.ExcelVersion = ExcelVersion.Excel2013;
+            var excelEngine = dataGrid.ExportToExcel(dataGrid.View, options);
+            var workBook = excelEngine.Excel.Workbooks[0];
+
+            SaveFileDialog sfd = new SaveFileDialog
+            {
+                FilterIndex = 2,
+                Filter = "Excel 97 to 2003 Files(*.xls)|*.xls|Excel 2007 to 2010 Files(*.xlsx)|*.xlsx|Excel 2013 File(*.xlsx)|*.xlsx"
+            };
+
+            if (sfd.ShowDialog() == true)
+            {
+                using (Stream stream = sfd.OpenFile())
+                {
+
+                    if (sfd.FilterIndex == 1)
+                        workBook.Version = ExcelVersion.Excel97to2003;
+
+                    else if (sfd.FilterIndex == 2)
+                        workBook.Version = ExcelVersion.Excel2010;
+
+                    else
+                        workBook.Version = ExcelVersion.Excel2013;
+                    workBook.SaveAs(stream);
+                }
+
+                //Message box confirmation to view the created workbook.
+
+                if (MessageBox.Show("Do you want to view the workbook?", "Workbook has been created",
+                                    MessageBoxButton.YesNo, MessageBoxImage.Information) == MessageBoxResult.Yes)
+                {
+
+                    //Launching the Excel file using the default Application.[MS Excel Or Free ExcelViewer]
+                    System.Diagnostics.Process.Start(sfd.FileName);
+                }
+            }
+        }
+
+        private void OnExportToPDF(object sender, RoutedEventArgs e)
+        {
+            var document = dataGrid.ExportToPdf();
+            SaveFileDialog sfd = new SaveFileDialog
+            {
+                Filter = "PDF Files(*.pdf)|*.pdf"
+            };
+
+            if (sfd.ShowDialog() == true)
+            {
+                using (Stream stream = sfd.OpenFile())
+                {
+                    document.Save(stream);
+                }
+
+                //Message box confirmation to view the created Pdf file.
+
+                if (MessageBox.Show("Do you want to view the Pdf file?", "Pdf file has been created",
+                                    MessageBoxButton.YesNo, MessageBoxImage.Information) == MessageBoxResult.Yes)
+                {
+
+                    //Launching the Pdf file using the default Application.
+                    System.Diagnostics.Process.Start(sfd.FileName);
+                }
+            }
+        }
+
+        private void OnPrinting(object sender, RoutedEventArgs e)
+        {
+            dataGrid.ShowPrintPreview();
+        }
+    }
+}
---
+++ Exporting_With_Filtering_Grouping/Model/OrderInfo.cs
@@ -0,0 +1,65 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Exporting_With_Filtering_Grouping
+{
+    public class OrderInfo : INotifyPropertyChanged
+    {
+        int orderID;
+        string customerId;
+        string country;
+        string customerName;
+        string shippingCity;
+
+        [Display(Name = "Order ID")]
+        public int OrderID
+        {
+            get { return orderID; }
+            set { orderID = value; }
+        }
+
+        [Display(Name = "Customer ID")]
+        public string CustomerID
+        {
+            get { return customerId; }
+            set { customerId = value; }
+        }
+
+        [Display(Name = "Customer Name")]
+        public string CustomerName
+        {
+            get { return customerName; }
+            set { customerName = value; }
+        }
+
+        public string Country
+        {
+            get { return country; }
+            set { country = value; }
+        }
+
+        [Display(Name = "Ship City")]
+        public string ShipCity
+        {
+            get { return shippingCity; }
+            set { shippingCity = value; }
+        }
+
+        public OrderInfo(int orderId, string customerName, string country, string customerId, string shipCity)
+        {
+            this.OrderID = orderId;
+            this.CustomerName = customerName;
+            this.Country = country;
+            this.CustomerID = customerId;
+            this.ShipCity = shipCity;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string PropertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(PropertyName));
+        }
+    }
+}
